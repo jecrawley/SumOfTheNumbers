d489c8fea14e2e4412dd00452e2cd7d00ebf75db
James Crawley <jamescrawley@James-ZCW-15.local>
2016-09-10 15:46:20 -0400

    Summed all the numbers

---
+++ src/main/java/crawley/james/SumOfTheNumbers/SumOfTheNumbers.java
@@ -0,0 +1,22 @@
+package crawley.james.SumOfTheNumbers;
+
+import java.util.Scanner;
+
+/**
+ * Created by jamescrawley on 9/9/16.
+ */
+public class SumOfTheNumbers {
+
+    public static void main(String[] args) {
+
+        SumOfNums sumOfNums = new SumOfNums();
+        int n;
+
+        System.out.println("This program sums up the numbers from 1 until a given value.");
+        System.out.println("Please input an integer: ");
+
+        n = sumOfNums.getValidNumberFromUser();
+        System.out.println(sumOfNums.sumNums(n));
+
+    }
+}
